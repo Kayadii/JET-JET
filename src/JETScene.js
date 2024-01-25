1677519ce2a963b7b11e194ff65d07cf0e7c2c27
Kayadii <bebekuyuk@gmail.com>
2024-01-25 20:36:56 +0800

    123456789

--- src/JETScene.js
+++ src/JETScene.js
@@ -1,24 +1,36 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-export default class JETScene extends Phaser.Scene{
+export default class JETScene extends Phaser.Scene {
+  constructor() {
+    super("JETScene");
+  }
 
-    constructor(){
-        super('JETScene')
-    }
+  init() {
+    this.player = undefined;
+  }
 
-    init(){
+  preload() {
+    // Background
+    this.load.image("bg", "images/Environment/Background.png");
+    this.load.image("bg-planet", "images/Environment/Planet Besar.png");
+    this.load.image("bg-stars", "images/Environment/Bintang.png");
+    this.load.image("bg-far-planets", "images/Environment/Planet Kecil.png");
+    this.load.spritesheet("player", "images/Ship/Ship.png",{
+                frameWidth : 48,
+                frameHeight : 48
+    })
+  }
 
-    }
+  create() {
+    // BACKGROUNDS
+    this.add.image(240, 320, "bg").setScale(3.5);
+    this.add.image(300, 400, "bg-far-planets");
+    this.add.image(100, 200, "bg-planet").setScale(1.5);
+    this.add.image(200, 310, "bg-stars").setScale(2);
+    this.player = this.physics.add.sprite(240, 557, "player").setScale(1.5);
+  }
 
-    preload(){
+  update() {
 
-    }
-
-    create(){
-
-    }
-
-    update(){
-        
-    }
+  }
 }
